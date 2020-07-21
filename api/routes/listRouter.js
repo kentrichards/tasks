70eb799fba0bd94ea24eef8abe8a1176cf07d27a
Kent Richards <kent-richards@outlook.com>
2020-07-21 19:24:27 -0300

    created route for creating a new list added simple VS Code Rest Client test for the route

--- api/routes/listRouter.js
+++ api/routes/listRouter.js
@@ -1,7 +1,19 @@
 const listRouter = require('express').Router();
+const List = require('../models/list');
 
 listRouter.get('/', (_request, response) => {
   response.send('default list route');
+});
+
+// Create a new list
+listRouter.post('/', async (request, response, next) => {
+  try {
+    const newList = new List({ name: request.body.name });
+    const result = await newList.save();
+    response.status(201).json(result);
+  } catch (exception) {
+    next(exception);
+  }
 });
 
 module.exports = listRouter;
