a5e9978f9e48d2fa6c80082d232fb6bd2ecc27e4
Kent Richards <kent-richards@outlook.com>
2020-07-21 20:10:51 -0300

    added route for fetching a single list TODO: figure out why CastError is not being caught by errorHandler

--- api/routes/lists.js
+++ api/routes/lists.js
@@ -16,4 +16,23 @@ listRouter.post('/', async (request, response, next) => {
   }
 });
 
+// Fetch an individual list
+listRouter.get('/:id', async (request, response, next) => {
+  try {
+    const list = await List.findById(request.params.id);
+
+    if (list) {
+      response.json(list);
+    } else {
+      // This error is thrown when a valid id is supplied that doesn't match a list
+      // If an invalid id is given, a CastError will be thrown and passed to errorHandler
+      // TODO: Figure out why errorHandler doesn't correctly detect the CastError and
+      // then make the error message it gives off more detailed
+      response.status(404).send({ error: `no list found with id ${request.params.id}` });
+    }
+  } catch (exception) {
+    next(exception);
+  }
+});
+
 module.exports = listRouter;
