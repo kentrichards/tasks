3d0961e4fe00ab18c4b5056afd6999300987672a
Kent Richards <kent-richards@outlook.com>
2020-07-22 13:44:36 -0300

    add wrapAsync for passing async errors to handler

--- api/middleware/wrapAsync.js
+++ api/middleware/wrapAsync.js
@@ -1,2 +1,8 @@
-// TODO: Research asynchronous errors in routes
-// https://thecodebarbarian.com/80-20-guide-to-express-error-handling
+// Used to execute all asynchronous middleware operations
+// This prevents UnhandledPromiseRejectionWarnings without
+// needing to wrap every async operation in a try/catch
+const wrapAsync = (fn) => (request, response, next) => {
+  Promise.resolve(fn(request, response, next)).catch(next);
+};
+
+module.exports = wrapAsync;
