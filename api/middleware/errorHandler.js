9a9fde4d47535c2d0ba6df43909369d7128849e2
Kent Richards <kent-richards@outlook.com>
2020-07-22 13:52:10 -0300

    refactored errorHandler to handle all errors

--- api/middleware/errorHandler.js
+++ api/middleware/errorHandler.js
@@ -1,22 +1,26 @@
 const logger = require('../utils/logger');
 
-const errorHandler = (error, _request, response, next) => {
+// '_next' is necessary for Express to recognize this an error handler
+// eslint-disable-next-line no-unused-vars
+const errorHandler = (error, _request, response, _next) => {
   logger.error(error.message);
+
+  let message = error.message || 'Internal server error';
+  let statusCode = error.statusCode || 500;
 
   // CastError is thrown when Mongoose fails to cast a value (e.g., an id)
   if (error.name === 'CastError') {
-    response.status(400).send({ error: 'malformed id' });
-    return;
+    message = 'malformed id';
+    statusCode = 400;
   }
 
   // Database entry failed to meet the constraints defined by the schema
   if (error.name === 'ValidationError') {
-    response.status(400).send({ error: 'database entry does not meet schema requirements' });
-    return;
+    message = 'database entry does not meet schema requirements';
+    statusCode = 400;
   }
 
-  // Pass the error to Express's default handler if we don't recognize it
-  next(error);
+  response.status(statusCode).json({ error: message });
 };
 
 module.exports = errorHandler;
