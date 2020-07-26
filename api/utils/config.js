cbc579ba91f2e2e908fca4e793ce10f24b348432
Kent Richards <kent-richards@outlook.com>
2020-07-26 14:12:16 -0300

    setup for api integration testing

--- api/utils/config.js
+++ api/utils/config.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 
-const { PORT, MONGODB_URI } = process.env;
+const { PORT } = process.env;
+let { MONGODB_URI } = process.env;
+
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URI = process.env.MONGODB_URI_TEST;
+}
 
 module.exports = { PORT, MONGODB_URI };
