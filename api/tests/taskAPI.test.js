d6e00d6f9023ef8507109cb8ecc906f9738ccc88
Kent Richards <kent-richards@outlook.com>
2020-07-27 11:35:15 -0300

    tests for adding a new task

--- api/tests/taskAPI.test.js
+++ api/tests/taskAPI.test.js
@@ -42,5 +42,41 @@ describe('fetching all tasks', () => {
   });
 });
 
+describe('adding new tasks', () => {
+  test('a valid task can be added', async () => {
+    const newTask = {
+      text: 'what i want to do today',
+      important: true,
+      listId: helper.initialTasks[0].listId,
+    };
+
+    // Add new task to the database and verify it worked
+    await api
+      .post('/api/tasks')
+      .send(newTask)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    // We should have more tasks than we started with
+    const tasksAtEnd = await helper.getTasks();
+    expect(tasksAtEnd).toHaveLength(helper.initialTasks.length + 1);
+
+    // The new task should have the correct text
+    const texts = tasksAtEnd.map((t) => t.text);
+    expect(texts).toContain(newTask.text);
+  });
+
+  test('invalid task is not added', async () => {
+    const invalidTask = { content: 'wrong format' };
+
+    // Server should reject our request
+    await api.post('/api/tasks').send(invalidTask).expect(400);
+
+    // Database should not have any new tasks
+    const tasksAtEnd = await helper.getTasks();
+    expect(tasksAtEnd).toHaveLength(helper.initialTasks.length);
+  });
+});
+
 // Clean-up when all tests have finished running
 afterAll(() => mongoose.connection.close());
