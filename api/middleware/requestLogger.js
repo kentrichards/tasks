b0cbf3e841f55bd10456cb952a1607cee01d3eb5
Kent Richards <kent-richards@outlook.com>
2020-07-30 13:45:22 -0300

    updated requestLogger to be more clear

--- api/middleware/requestLogger.js
+++ api/middleware/requestLogger.js
@@ -1,10 +1,10 @@
 const logger = require('../utils/logger');
 
 const requestLogger = (request, _response, next) => {
+  logger.info('---');
   logger.info('Method:', request.method);
   logger.info('Path:', request.path);
   logger.info('Body:', request.body);
-  logger.info('---');
 
   next();
 };
