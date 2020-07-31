4bebabc71fc3e19e649dd4fcd0dbe462855a66ab
Kent Richards <kent-richards@outlook.com>
2020-07-31 15:03:50 -0300

    added user delete route

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,8 @@
 const userRouter = require('express').Router();
-const { createUser, fetchUser } = require('../controllers/users');
+const { createUser, fetchUser, deleteUser } = require('../controllers/users');
 
 userRouter.post('/', createUser);
 userRouter.get('/:id', fetchUser);
+userRouter.delete('/:id', deleteUser);
 
 module.exports = userRouter;
